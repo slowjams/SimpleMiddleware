fb6ea959678aa43755b4da396f9c6daa38d5e061
slowjams <lxdthriller@gmail.com>
2020-11-18 09:27:40 +1100

    init

---
+++ MiddlewareProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace MiddlewareProject
+{
+    class Program
+    {
+        static void Main(string[] args) {
+
+            Try(LambdaFirst);
+            Wrap(LambdaSecond);
+            //Wrap(First);
+            //Wrap(Second);
+
+        }
+
+        public static void First() {
+            Console.WriteLine("executing first function");
+        }
+
+        public static void Second() {
+            Console.WriteLine("executing second function");
+        }
+
+        public static void LambdaFirst() {
+            Wrap(First);
+        }
+
+        public static void LambdaSecond() {
+            Try(Second);
+        }
+
+        public static void Wrap(Action function) {
+            Console.WriteLine("start");
+            function();
+            Console.WriteLine("end");
+        }
+
+        public static void Try(Action function) {
+            try {
+                Console.WriteLine("trying");
+                function();
+            } catch(Exception)
+            {
+
+            }
+        }
+    }
+}
